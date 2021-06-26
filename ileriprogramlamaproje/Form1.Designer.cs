616b1561bfbbf78f63d85ac9e58ab2c2d30e5f41
lafsa13 <86532637+lafsa13@users.noreply.github.com>
2021-06-26 21:29:06 +0300

    Add files via upload

---
+++ ileriprogramlamaproje/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace ileriprogramlamaproje
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dosyaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dosyaAç_tool = new System.Windows.Forms.ToolStripMenuItem();
+            this.dosyaKaydet_tool = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkış_tool = new System.Windows.Forms.ToolStripMenuItem();
+            this.işlemlerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.griyeÇevir_tool = new System.Windows.Forms.ToolStripMenuItem();
+            this.negatifeÇevir_tool = new System.Windows.Forms.ToolStripMenuItem();
+            this.sıkıştır_tool = new System.Windows.Forms.ToolStripMenuItem();
+            this.netleştir_tool = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.sikistir_text = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaToolStripMenuItem,
+            this.işlemlerToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1402, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            this.dosyaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaAç_tool,
+            this.dosyaKaydet_tool,
+            this.çıkış_tool});
+            this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.dosyaToolStripMenuItem.Text = "Dosya";
+            // 
+            // dosyaAç_tool
+            // 
+            this.dosyaAç_tool.Name = "dosyaAç_tool";
+            this.dosyaAç_tool.Size = new System.Drawing.Size(175, 26);
+            this.dosyaAç_tool.Text = "Dosya Aç";
+            this.dosyaAç_tool.Click += new System.EventHandler(this.dosyaAç_tool_Click);
+            // 
+            // dosyaKaydet_tool
+            // 
+            this.dosyaKaydet_tool.Name = "dosyaKaydet_tool";
+            this.dosyaKaydet_tool.Size = new System.Drawing.Size(175, 26);
+            this.dosyaKaydet_tool.Text = "Dosya Kaydet";
+            this.dosyaKaydet_tool.Click += new System.EventHandler(this.dosyaKaydet_tool_Click);
+            // 
+            // çıkış_tool
+            // 
+            this.çıkış_tool.Name = "çıkış_tool";
+            this.çıkış_tool.Size = new System.Drawing.Size(175, 26);
+            this.çıkış_tool.Text = "Çıkış";
+            this.çıkış_tool.Click += new System.EventHandler(this.çıkış_tool_Click);
+            // 
+            // işlemlerToolStripMenuItem
+            // 
+            this.işlemlerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.griyeÇevir_tool,
+            this.negatifeÇevir_tool,
+            this.sıkıştır_tool,
+            this.netleştir_tool});
+            this.işlemlerToolStripMenuItem.Name = "işlemlerToolStripMenuItem";
+            this.işlemlerToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.işlemlerToolStripMenuItem.Text = "İşlemler";
+            // 
+            // griyeÇevir_tool
+            // 
+            this.griyeÇevir_tool.Name = "griyeÇevir_tool";
+            this.griyeÇevir_tool.Size = new System.Drawing.Size(179, 26);
+            this.griyeÇevir_tool.Text = "Griye Çevir";
+            this.griyeÇevir_tool.Click += new System.EventHandler(this.griyeÇevir_tool_Click);
+            // 
+            // negatifeÇevir_tool
+            // 
+            this.negatifeÇevir_tool.Name = "negatifeÇevir_tool";
+            this.negatifeÇevir_tool.Size = new System.Drawing.Size(179, 26);
+            this.negatifeÇevir_tool.Text = "Negatife Çevir";
+            this.negatifeÇevir_tool.Click += new System.EventHandler(this.negatifeÇevir_tool_Click);
+            // 
+            // sıkıştır_tool
+            // 
+            this.sıkıştır_tool.Name = "sıkıştır_tool";
+            this.sıkıştır_tool.Size = new System.Drawing.Size(179, 26);
+            this.sıkıştır_tool.Text = "Sıkıştır";
+            this.sıkıştır_tool.Click += new System.EventHandler(this.sıkıştır_tool_Click);
+            // 
+            // netleştir_tool
+            // 
+            this.netleştir_tool.Name = "netleştir_tool";
+            this.netleştir_tool.Size = new System.Drawing.Size(179, 26);
+            this.netleştir_tool.Text = "Netleştir";
+            this.netleştir_tool.Click += new System.EventHandler(this.netleştir_tool_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(21, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(433, 490);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.pictureBox2.Location = new System.Drawing.Point(906, 67);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(433, 490);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.BackgroundImage = global::ileriprogramlamaproje.Properties.Resources.arkaplan;
+            this.panel1.Controls.Add(this.labelControl1);
+            this.panel1.Controls.Add(this.sikistir_text);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.panel1.Location = new System.Drawing.Point(0, 23);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1400, 600);
+            this.panel1.TabIndex = 3;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(527, 101);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(171, 16);
+            this.labelControl1.TabIndex = 4;
+            this.labelControl1.Text = "Sıkıştırmak İstediğiniz Miktar :";
+            this.labelControl1.Click += new System.EventHandler(this.labelControl1_Click);
+            // 
+            // sikistir_text
+            // 
+            this.sikistir_text.BackColor = System.Drawing.SystemColors.Control;
+            this.sikistir_text.Location = new System.Drawing.Point(713, 98);
+            this.sikistir_text.Name = "sikistir_text";
+            this.sikistir_text.Size = new System.Drawing.Size(100, 22);
+            this.sikistir_text.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(1402, 607);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Görüntü İşleme";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dosyaAç_tool;
+        private System.Windows.Forms.ToolStripMenuItem dosyaKaydet_tool;
+        private System.Windows.Forms.ToolStripMenuItem çıkış_tool;
+        private System.Windows.Forms.ToolStripMenuItem işlemlerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem griyeÇevir_tool;
+        private System.Windows.Forms.ToolStripMenuItem negatifeÇevir_tool;
+        private System.Windows.Forms.ToolStripMenuItem sıkıştır_tool;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ToolStripMenuItem netleştir_tool;
+        private System.Windows.Forms.Panel panel1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private System.Windows.Forms.TextBox sikistir_text;
+    }
+}
+
---
+++ ileriprogramlamaproje/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace ileriprogramlamaproje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("Lütfen Yukarıdan Menüden Dosyaya Tıklayarak Fotoğrafınızı Yükleyiniz.");
+        }
+        private void DosyaAc()
+        {
+            OpenFileDialog dosya = new OpenFileDialog();
+            dosya.Filter = "Resim Dosyası |*.jpeg| video|*.avi| Tüm Dosyalar |*.*";
+            dosya.Title = "";
+            dosya.ShowDialog();
+            string DosyaYolu = dosya.FileName;
+            pictureBox1.ImageLocation = DosyaYolu;
+        }
+        private Bitmap GriYap(Bitmap cikisresmi)
+        {
+            for(int i = 0; i < cikisresmi.Height - 1; i++)
+            {
+                for(int j=0; j < cikisresmi.Width - 1; j++)
+                {
+                    int deger = (cikisresmi.GetPixel(j, i).R + cikisresmi.GetPixel(j, i).G + cikisresmi.GetPixel(j, i).B * 0) / 3;
+                    Color renk;
+                    renk = Color.FromArgb(deger, deger, deger);
+                    cikisresmi.SetPixel(j, i, renk);
+                }
+            }
+            return cikisresmi;
+        }
+        private void DosyaKaydet()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "JPEG files(*.jpeg|*.jpeg";
+            if(DialogResult.OK == sfd.ShowDialog())
+            {
+                this.pictureBox2.Image.Save(sfd.FileName, ImageFormat.Jpeg);
+                
+            }
+        }
+        private void Negatif()
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("önce bir resim seçin");
+            }
+            else
+            {
+                int i, j;
+                Color r;
+                Bitmap bmp = new Bitmap(pictureBox1.Image);
+
+                for(i=0; i <= bmp.Width - 1; i++)
+                {
+                    for(j=0; j <= bmp.Height - 1; j++)
+                    {
+                        r = bmp.GetPixel(i, j);
+                        r = Color.FromArgb(r.A, (byte)-r.R, (byte)-r.G, (byte)-r.B);
+                        bmp.SetPixel(i, j, r);
+                        
+                    }
+                }
+                pictureBox2.Image = bmp;
+            }
+        }
+        private void Cikis()
+        {
+            Application.Exit();
+        }
+        private void Piksel()
+        {
+            int xof, yof;
+            var pikselBoyutu = Convert.ToInt16(sikistir_text.Text);
+
+            var orjinalGoruntu = new Bitmap(pictureBox1.Image);
+            var goruntuGenislik = orjinalGoruntu.Width;
+            var goruntuYukseklik = orjinalGoruntu.Height;
+
+            var piksellestirilmisGoruntu = new Bitmap(goruntuGenislik, goruntuYukseklik);
+
+            for(var i = 0; i < goruntuGenislik; i += pikselBoyutu)
+            {
+                for(var j = 0; j < goruntuYukseklik; j += pikselBoyutu)
+                {
+                    xof = yof = pikselBoyutu / 2;
+
+                    if (i + xof >= goruntuGenislik)
+                    {
+                        xof = goruntuGenislik - i - 1;
+                    }
+                    if(j+yof >= goruntuYukseklik)
+                    {
+                        yof = goruntuYukseklik - j - 1;
+                    }
+                    var piksel = orjinalGoruntu.GetPixel(i + xof, j + yof);
+
+                    for(var x = i; x < i + pikselBoyutu && x < goruntuGenislik; x++)
+                    {
+                        for(var y = j; y < j + pikselBoyutu && y < goruntuYukseklik; y++)
+                        {
+                            piksellestirilmisGoruntu.SetPixel(x, y, piksel);
+                        }
+                    }
+                }
+            }
+            pictureBox2.Image = piksellestirilmisGoruntu;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void griyeÇevir_tool_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(pictureBox1.Image);
+            Bitmap gri = GriYap(image);
+            pictureBox2.Image = gri;
+        }
+
+        private void negatifeÇevir_tool_Click(object sender, EventArgs e)
+        {
+            Negatif();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void sıkıştır_tool_Click(object sender, EventArgs e)
+        {
+            int i;
+            if (sikistir_text.Text == "")
+            {
+                i = 0;
+                MessageBox.Show("Sıkıştırmak İstediğiniz Miktarı Boş Olamaz.");
+            }
+            else { i = 1; }
+            if (i == 1)
+            {
+                Piksel();
+            }
+        }
+
+        private void dosyaAç_tool_Click(object sender, EventArgs e)
+        {
+            DosyaAc();
+        }
+
+        private void dosyaKaydet_tool_Click(object sender, EventArgs e)
+        {
+            DosyaKaydet();
+        }
+
+        private void çıkış_tool_Click(object sender, EventArgs e)
+        {
+            Cikis();
+        }
+
+
+        private void netleştir_tool_Click(object sender, EventArgs e)
+        {
+            netlestir2();
+        }
+        private void netlestir2()
+        {
+            Color OkunanRenk;
+            Bitmap GirisResmi, CikisResmi;
+            GirisResmi = new Bitmap(pictureBox1.Image);
+            int ResimGenisligi = GirisResmi.Width;
+            int ResimYuksekligi = GirisResmi.Height;
+            CikisResmi = new Bitmap(ResimGenisligi, ResimYuksekligi);
+            int SablonBoyutu = 3;
+            int ElemanSayisi = SablonBoyutu * SablonBoyutu;
+            int x, y, i, j, toplamR, toplamG, toplamB;
+            int R, G, B;
+            int[] Matris = { 0, -2, 0, -2, 11, -2, 0, -2, 0 };
+            int MatrisToplami = 0 + -2 + 0 + -2 + 11 + -2 + 0 + -2 + 0;
+            for (x = (SablonBoyutu - 1) / 2; x < ResimGenisligi - (SablonBoyutu - 1) / 2; x++) 
+ {
+                for (y = (SablonBoyutu - 1) / 2; y < ResimYuksekligi - (SablonBoyutu - 1) / 2; y++)
+                {
+                    toplamR = 0;
+                    toplamG = 0;
+                    toplamB = 0;
+
+                    int k = 0; 
+                    for (i = -((SablonBoyutu - 1) / 2); i <= (SablonBoyutu - 1) / 2; i++)
+                    {
+                        for (j = -((SablonBoyutu - 1) / 2); j <= (SablonBoyutu - 1) / 2; j++)
+                        {
+                            OkunanRenk = GirisResmi.GetPixel(x + i, y + j);
+                            toplamR = toplamR + OkunanRenk.R * Matris[k];
+                            toplamG = toplamG + OkunanRenk.G * Matris[k];
+                            toplamB = toplamB + OkunanRenk.B * Matris[k];
+                            k++;
+                        }
+                    }
+                    R = toplamR / MatrisToplami;
+                    G = toplamG / MatrisToplami;
+                    B = toplamB / MatrisToplami;
+
+                    if (R > 255) R = 255;
+                    if (G > 255) G = 255;
+                    if (B > 255) B = 255;
+                    if (R < 0) R = 0;
+                    if (G < 0) G = 0;
+                    if (B < 0) B = 0;
+
+                    CikisResmi.SetPixel(x, y, Color.FromArgb(R, G, B));
+                }
+            }
+            pictureBox2.Image = CikisResmi;
+        }
+
+        private void labelControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
